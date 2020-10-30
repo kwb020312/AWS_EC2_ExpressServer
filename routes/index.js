1dc1a4ffd915a734d739be9104fbbd23e00a3d5d
kimwoobin <kwb020312@naver.com>
2020-10-30 10:01:34 +0900

    new Fixed Update

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let Menu
-  Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c3348cd4353d49b595fdd38c0eb035e1&Type=json&pIndex=1&pSize=1&SD_SCHUL_CODE=7530336&ATPT_OFCDC_SC_CODE=J10&MLSV_YMD=20201029`)
+  const Today = new Date()
+  Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c3348cd4353d49b595fdd38c0eb035e1&Type=json&pIndex=1&pSize=1&SD_SCHUL_CODE=7530336&ATPT_OFCDC_SC_CODE=J10&MLSV_YMD=${Today.getFullYear()}${(Today.getMonth()+1).toString().length === 2 ? Today.getMonth()+1 : '0'+(Today.getMonth()+1)}${Today.getDate().toString().length === 2 ? Today.getDate() : '0'+Today.getDate()}`)
   .then(res => res.data.mealServiceDietInfo[1].row[0].DDISH_NM)
   .then(res => res.replace(/(<([^>]+)>)/ig,'쉼').match(/[가-힣*$]/g).join("").split("").join("").replace(/쉼/g,','))
   .then(data => {
-    res.render('index', { title: 'Express' , Menu:data });
+    res.render('index', { title: '삼일상업고등학교 급식' , Menu:data });
   })
 });
 
